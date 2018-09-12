08eff8dfae086f11638ec0ce0d96801da7d49d55
git <qiudanyan86@163.com>
2018-09-12 11:54:23 +0800

    for the melaleuca mac

--- tinyheart/js/background.js
+++ tinyheart/js/background.js
@@ -1,12 +1,5 @@
 function drawBackground() {
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-  ctx2.
-=======
 ctx2.drawImage(bgPic,0,0,canwidth,canhg)
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
-=======
-ctx2.drawImage(bgPic,0,0,canwidth,canhg)
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
+
 }
--- tinyheart/js/main.js
+++ tinyheart/js/main.js
@@ -4,36 +4,22 @@ var ctx1
 var ctx2
 var lastTime
 var daltaTime
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
+
 var canwidth
 var canhg
 var ane
 var fruit
 var bgPic = new Image()
-<<<<<<< HEAD
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
-=======
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
+
 document.body.onload = game;
 
 function game (){
   init()
   lastTime = Date.now()
   daltaTime = 0
-<<<<<<< HEAD
-<<<<<<< HEAD
+  gameloop()
 
-  // gameloop()
-=======
-  gameloop()
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
-=======
-  gameloop()
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
+
 }
 
 
@@ -42,11 +28,7 @@ function init() {
   ctx1= can1.getContext("2d")
   can2 = document.querySelector('#canvas2')
   ctx2= can2.getContext("2d")
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
+
   bgPic.src = './src/background.jpg'
   canwidth = can1.width
   canhg = can1.height
@@ -54,10 +36,7 @@ function init() {
   ane.init()
   fruit = new fruitObj()
   fruit.init()
-<<<<<<< HEAD
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
-=======
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
+
   // canvas context
 }
 
@@ -67,18 +46,8 @@ function gameloop(){
    var now = Date.now()
    deltaTime = now - lastTime
    lastTime = Date.now()
-<<<<<<< HEAD
-<<<<<<< HEAD
-   console.log(deltaTime)
-=======
-=======
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
-
    drawBackground()
    ane.draw()
    fruit.draw()
-<<<<<<< HEAD
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
-=======
->>>>>>> 9670231aba166e8adec9ec842d448cc45152bcb4
+
  }
